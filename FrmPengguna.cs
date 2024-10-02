22ca70b7bac4757c8af003ed53fcf3002ad4a221
Aprillia Mononutu <monoapril7@gmail.com>
2024-10-02 20:56:11 +0800

    Add files via upload

---
+++ FrmPengguna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoneksiDatabase
+{
+    public partial class FrmPengguna : Form
+    {
+        public FrmPengguna()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPengguna_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
